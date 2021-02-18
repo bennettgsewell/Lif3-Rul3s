15545402dfac8dbab261ea80ac239094c82883a8
Bennett Sewell <bennettgsewell@gmail.com>
2021-02-17 23:02:58 -0500

    Made my first Android Notification!

--- app/src/main/java/com/bucketsofcrazy/lif3rul3s/MainActivity.kt
+++ app/src/main/java/com/bucketsofcrazy/lif3rul3s/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.bucketsofcrazy.lif3rul3s
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        fun channel() : String = "Lif3Rul3s"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +27,36 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        createNotificationChannel()
+
+        val builder : NotificationCompat.Builder = NotificationCompat.Builder(this, channel())
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Test Title")
+                .setContentText("The text!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(0, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        //If Oreo or higher, because these classes didn't exist before.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            //Design the notification channel
+            val name = channel()
+            val descriptionText = "Lif3 Rul3s is an app"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channel(), name, importance).apply {
+                description = descriptionText
+            }
+
+            //Tell Android that we're declaring a notification channel
+            val notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
